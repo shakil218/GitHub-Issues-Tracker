feacdceda88d665b0e84b7962f73273ab3751bdc
shakil218 <rabiulhasanshakil218@gmail.com>
2026-03-06 23:31:42 +0600

    Login functionality implement done.

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,16 @@
+const loginForm = document.getElementById("login-form");
 
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // prevents the page from reloading
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+    // Redirect to dashboard
+    window.location.replace("dashboard.html");
+  } else {
+    // show error
+    alert("Invalid login credentials");
+  }
+});
